298c7291a6bf1f9fd3269b66ea78f4a8e052eee1
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-01-04 19:45:17 -0600

    fix text-overflow

--- components/Heading.js
+++ components/Heading.js
@@ -4,7 +4,7 @@ export default function Heading({
 }) {
     return (
         <div className="w-3/4 tracking-widest">
-            <h1 className="font-rubik font-bold text-6xl text-white uppercase mb-2">{heading}</h1>
+            <h1 className="font-rubik font-bold text-5xl text-white uppercase mb-2">{heading}</h1>
             <h2 className="font-lato italic uppercase text-3xl">{subheading}</h2>
         </div>
     )
