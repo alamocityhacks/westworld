59e553ac7dfdae81738a7a619c9af95eb8dd1347
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-01-06 06:28:40 +0000

    🚀 add registration page

---
+++ pages/register.js
@@ -0,0 +1,5 @@
+export default function RegisterPage() {
+    return (
+        <iframe className="airtable-embed" src="https://airtable.com/embed/shr1x94ouUthItwkJ?backgroundColor=orange" frameBorder={0} onmousewheel width="100%" height={533} style={{background: 'transparent', border: '1px solid #ccc'}} />
+    )
+}
